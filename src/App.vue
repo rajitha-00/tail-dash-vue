ec867d43dd1e3eb939a99f4450a702104f8d5016
rajitha-00 <rajithatw07102@gmail.com>
2023-05-04 20:26:31 +0100

    Tailwind Configued

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="bg-gray-600">Hello Dashboard</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+
   }
 }
 </script>
